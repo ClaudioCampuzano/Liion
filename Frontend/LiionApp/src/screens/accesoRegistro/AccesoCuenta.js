552cc65ef2b4d788c8e89098f7c8ad44ab69cf77
claudio <claudio.campuzano.b@gmail.com>
2021-08-11 01:12:21 -0400

    se agrega verificacion de email

--- Frontend/LiionApp/src/screens/accesoRegistro/AccesoCuenta.js
+++ Frontend/LiionApp/src/screens/accesoRegistro/AccesoCuenta.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Keyboard } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Keyboard,
+} from "react-native";
+
 
 import Layout from "../../components/Layout";
 
@@ -11,29 +18,48 @@ import { COLORS } from "../../constants/styleThemes";
 const AccesoCuenta = () => {
   const [valueEmail, setValueEmail] = useState("");
   const [valueContraseña, setValueContraseña] = useState("");
+  const [error, setError] = useState(null);
   const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    if (valueEmail != '') {
+      if (!validate(valueEmail))
+        setError('Formato de email incorrecto')
+      else
+        setError(null)
+    }
+
+  }, [isKeyboardVisible]);
+
 
   useEffect(() => {
     const keyboardDidShowListener = Keyboard.addListener(
       'keyboardDidShow',
       () => {
-        setKeyboardVisible(true); 
+        setKeyboardVisible(true); // or some other action
       }
     );
     const keyboardDidHideListener = Keyboard.addListener(
       'keyboardDidHide',
       () => {
-        setKeyboardVisible(false);
+        setKeyboardVisible(false); // or some other action
       }
     );
-
     return () => {
       keyboardDidHideListener.remove();
       keyboardDidShowListener.remove();
     };
   }, []);
 
-  //const [error, setError] = useState(null)
+  const validate = (text) => {
+    const reg = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    if (reg.test(text) === false) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
   return (
     <Layout>
       <View>
@@ -43,6 +69,7 @@ const AccesoCuenta = () => {
           style={styles.input}
           label="Email"
           value={valueEmail}
+          errorText={error}
           onChangeText={(text) => setValueEmail(text)}
         />
         <InputLiion
@@ -57,16 +84,14 @@ const AccesoCuenta = () => {
             <Text style={styles.text_3}>¿Olvidaste tu contraseña?</Text>
           </View>
         </TouchableOpacity>
-        <View style={{paddingTop: 220 }}>
-        <ButtonLiion
-          title="Ingresar"
-          styleView={styles.button}
-          styleText={{ margin: -10 }}
-        />
+        <View style={{ paddingTop: 220 }}>
+          <ButtonLiion
+            title="Ingresar"
+            styleView={styles.button}
+            styleText={{ margin: -10 }}
+          />
+        </View>
       </View>
-      </View>
-      
-
     </Layout>
   );
 };
