87ac476e033ca6a333498fc5b18574c7bc125671
CarlosE <carloxhunter>
2021-11-02 20:43:25 -0300

    se divide la pantalla 5 en flexes

--- Frontend/LiionApp/src/screens/createTrip/CreateStepFive.js
+++ Frontend/LiionApp/src/screens/createTrip/CreateStepFive.js
@@ -10,24 +10,40 @@ const CreateStepFive = ({ navigation, route }) => {
     const titulo = "¡Creación de viaje realizada!";
     const subTitulo =
       "Tu creación de viaje fue generada exitosamente.\nPara chequear el estatus de tu\nviaje chequéalo en Mis viajes\n(conductor) en el home.";
-    const initialRoute = 'CreateStepOne'
+    const initialRoute = "CreateStepOne";
     navigation.navigate("SucessScreen", {
       titulo: titulo,
       subTitulo: subTitulo,
-      initialRoute: initialRoute
+      initialRoute: initialRoute,
     });
   };
   return (
     <Layout>
-      <View>
-        <Text>ETAPA CINCO</Text>
-      </View>
-      <View style={styles.buttonView}>
-        <ButtonLiion
-          title="Confirmar viaje"
-          styleView={styles.button}
-          onPress={() => checkValidator()}
-        />
+      <View style={styles.container}>
+        <View style={styles.contentFive}>
+          <View style={styles.titleContainer}>
+            <Text>ETAPA CINCO</Text>
+          </View>
+          <View style={styles.routeContainer}>
+          <Text>ETAPA CINCO</Text>
+          </View>
+          <View style={styles.vehicleContainer}>
+          <Text>ETAPA CINCO</Text>
+          </View>
+          <View style={styles.priceContainer}>
+          <Text>ETAPA CINCO</Text>
+          </View>
+          <View style={styles.bagsContainer}>
+          <Text>ETAPA CINCO</Text>
+          </View>
+        </View>
+        <View style={styles.buttonView}>
+          <ButtonLiion
+            title="Confirmar viaje"
+            styleView={styles.button}
+            onPress={() => checkValidator()}
+          />
+        </View>
       </View>
     </Layout>
   );
@@ -40,11 +56,39 @@ const styles = StyleSheet.create({
     flex: 1,
     height: hp("23%"),
     justifyContent: "flex-end",
-    paddingBottom: hp("8%"),
+    paddingBottom: hp("5%"),
+    backgroundColor: "green",
   },
   button: {
     width: wp("78.6%"),
     height: hp("4.8%"),
     alignSelf: "center",
   },
+  contentFive: {
+    flex: 9,
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  titleContainer: {
+    backgroundColor: "green",
+    flex: 1.1,
+  },
+  routeContainer:{
+    backgroundColor:"yellow",
+    flex:0.9,
+  },
+  vehicleContainer:{
+    backgroundColor:"green",
+    flex:0.7,
+  },
+  priceContainer:{
+    backgroundColor:"yellow",
+    flex:1,
+  },
+  bagsContainer:{
+    backgroundColor:"red",
+    flex:1.2,
+  }
 });
