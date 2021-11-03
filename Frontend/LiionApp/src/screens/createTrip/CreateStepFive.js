76de236104bb78e0d3fec9812cdd52e8fbdc6e34
CarlosE <carloxhunter>
2021-11-03 17:04:56 -0300

    se agrega secicon ruta y vehiculo

--- Frontend/LiionApp/src/screens/createTrip/CreateStepFive.js
+++ Frontend/LiionApp/src/screens/createTrip/CreateStepFive.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useContext, useEffect, useRef } from "react";
+import { StyleSheet, Text, View, Image} from "react-native";
 
 import Layout from "../../components/Layout";
 import ButtonLiion from "../../components/ButtonLiion";
 import { COLORS, hp, wp } from "../../constants/styleThemes";
+import ShowTravel from "../../components/ShowTravel";
 
 const CreateStepFive = ({ navigation, route }) => {
   const checkValidator = () => {
@@ -17,18 +18,39 @@ const CreateStepFive = ({ navigation, route }) => {
       initialRoute: initialRoute,
     });
   };
+  const vars = useRef({
+    origin:"Rancagua, Region de Ohiggins",
+    destiny:"San Fernando, Region de Ohiggins",
+    nOfSeats:3,
+    pricerPerSeat:4000,
+    nofBags:[1,1,1],
+    vehicle:{model:"Tesla Model X", color:"Blanco", patente:"ABCD12"}
+  })
   return (
     <Layout>
       <View style={styles.container}>
         <View style={styles.contentFive}>
           <View style={styles.titleContainer}>
-            <Text>ETAPA CINCO</Text>
+            <Text style={styles.titleStyle}>Confirmación de creacion de viaje</Text>
           </View>
           <View style={styles.routeContainer}>
-          <Text>ETAPA CINCO</Text>
+          <Text style={styles.rutaStyle} >Ruta</Text>
+          <ShowTravel
+                style={styles.inputLocation}
+                timeStart="16:00"
+                timeEnd="19:00"
+                labelO={vars.current.origin}
+                labelD={vars.current.destiny}
+                dirTextSize={wp("2.5%")}
+              />
           </View>
           <View style={styles.vehicleContainer}>
-          <Text>ETAPA CINCO</Text>
+          <Image source={require('../../../assets/images/teslaX.png')} style={styles.teslaImage}/>
+          <View>
+          <Text style={styles.vehicleModelTitle}> {vars.current.vehicle.model}</Text>
+          <Text style={styles.vehicleModelColor}>{vars.current.vehicle.patente}</Text>
+          <Text style={styles.vehicleModelColor}>{vars.current.vehicle.color}</Text>
+          </View>
           </View>
           <View style={styles.priceContainer}>
           <Text>ETAPA CINCO</Text>
@@ -74,6 +96,23 @@ const styles = StyleSheet.create({
   titleContainer: {
     backgroundColor: "green",
     flex: 1.1,
+    justifyContent:"center",
+    alignItems:"center",
+    
+    
+  },
+  titleStyle:{
+    fontSize:wp("8%"),
+    color:COLORS.TURKEY,
+    textAlign:"center",
+    marginHorizontal:wp("5%"),
+    fontFamily: "Gotham-SSm-Medium",
+  },
+  rutaStyle:{
+    fontSize:wp("5%"),
+    color:COLORS.TURKEY,
+    textAlign:"left",
+    fontFamily: "Gotham-SSm-Bold",
   },
   routeContainer:{
     backgroundColor:"yellow",
@@ -82,6 +121,27 @@ const styles = StyleSheet.create({
   vehicleContainer:{
     backgroundColor:"green",
     flex:0.7,
+    display:"flex",
+    flexDirection:"row",
+    justifyContent:"center",
+    alignItems:"center",
+    borderTopWidth:1,
+    borderBottomWidth:1,
+    borderColor:COLORS.GRAY,
+  },
+  vehicleModelTitle:{
+    fontFamily: "Gotham-SSm-Medium",
+    fontSize:wp("4%"),
+  },
+  vehicleModelColor:{
+    fontFamily: "Gotham-SSm-Medium",
+    fontSize:wp("3.5%"),
+    marginLeft:wp("1.5%"),
+  },
+  teslaImage:{
+    width:wp("25%"),
+    height:hp("7%"),
+    marginRight:wp("2%"),
   },
   priceContainer:{
     backgroundColor:"yellow",
@@ -90,5 +150,11 @@ const styles = StyleSheet.create({
   bagsContainer:{
     backgroundColor:"red",
     flex:1.2,
-  }
+  },
+  inputLocation: {
+    width: wp("78.6%"),
+    height: hp("12%"),
+    alignSelf: "center",
+    marginTop:-1*wp("2%"),
+  },
 });
